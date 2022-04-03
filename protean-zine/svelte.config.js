faca88bc6e20752a578d065ec73275099dd930d3
TheDolentCity <nardogamedev@outlook.com>
2022-04-02 21:12:55 -0700

    Fixed package

--- protean-zine/svelte.config.js
+++ protean-zine/svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
